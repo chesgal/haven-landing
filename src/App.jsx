097cb0186192d058901d26eff0cb31d06bb4f07e
CJ <>
2023-08-27 21:12:13 +1200

    Set up font and website title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-krub font-bold underline">
         Hello world!
       </h1>
     </>
